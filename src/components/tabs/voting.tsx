14ea7ef2e41ea11d7658c65dbaea725ddec0cd90
Jason Schrader <whoabuddy@users.noreply.github.com>
2023-09-01 17:53:45 -0700

    feat: add accordion template for voting proposals

--- src/components/tabs/voting.tsx
+++ src/components/tabs/voting.tsx
@@ -1,7 +1,145 @@
-import { Text } from "@chakra-ui/react";
+import {
+  Accordion,
+  AccordionButton,
+  AccordionItem,
+  AccordionPanel,
+  Badge,
+  Box,
+  Link,
+  Stack,
+  Stat,
+  StatLabel,
+  StatNumber,
+  Text,
+} from "@chakra-ui/react";
 
 function Voting() {
-  return <Text>Voting Proposals</Text>;
+  return (
+    <Accordion allowToggle>
+      <AccordionItem id="vote1">
+        <h2>
+          <AccordionButton>
+            <Box
+              as="span"
+              fontWeight="bold"
+              flex="1"
+              display="flex"
+              justifyContent="space-between"
+              alignItems="center"
+              textAlign="left"
+            >
+              <Text>Vote 1: Update emissions schedule</Text>
+              <Badge colorScheme="green" p={[2, 4]}>
+                Passed
+              </Badge>
+            </Box>
+          </AccordionButton>
+        </h2>
+        <AccordionPanel pb={4}>
+          <VoteContent />
+        </AccordionPanel>
+      </AccordionItem>
+    </Accordion>
+  );
+}
+
+function VoteContent() {
+  return (
+    <Stack spacing={4}>
+      <Box textAlign={["left", "center"]} bg="gray.900" p={4}>
+        <Stack direction={["column", "row"]} justifyContent="space-between">
+          <Stat>
+            <StatLabel>MIA Cycles</StatLabel>
+            <StatNumber>12, 13</StatNumber>
+          </Stat>
+          <Stat>
+            <StatLabel>NYC Cycles</StatLabel>
+            <StatNumber>6, 7</StatNumber>
+          </Stat>
+        </Stack>
+        <Stack direction={["column", "row"]} justifyContent="space-between">
+          <Stat>
+            <StatLabel>Yes Vote Count</StatLabel>
+            <StatNumber>135</StatNumber>
+          </Stat>
+          <Stat>
+            <StatLabel>No Vote Count</StatLabel>
+            <StatNumber>1</StatNumber>
+          </Stat>
+        </Stack>
+        <Stack direction={["column", "row"]} justifyContent="space-between">
+          <Stat>
+            <StatLabel>Yes Vote Total</StatLabel>
+            <StatNumber>1,500,875,294</StatNumber>
+          </Stat>
+          <Stat>
+            <StatLabel>No Vote Total</StatLabel>
+            <StatNumber>5,204,049</StatNumber>
+          </Stat>
+        </Stack>
+      </Box>
+      <Stack direction={["column", "row"]} justifyContent="space-between">
+        <Text fontWeight="bold">Related CCIPs:</Text>
+        <Box>
+          <Link href="#your-link-for-CCIP-008" isExternal>
+            CCIP-008
+          </Link>
+          ,
+          <Link href="#your-link-for-CCIP-009" ml={2} isExternal>
+            CCIP-009
+          </Link>
+          ,
+          <Link href="#your-link-for-CCIP-010" ml={2} isExternal>
+            CCIP-010
+          </Link>
+        </Box>
+      </Stack>
+      <Stack direction={["column", "row"]} justifyContent="space-between">
+        <Text fontWeight="bold">Related Contracts:</Text>
+        <Link href="#your-link-for-contract" isExternal>
+          citycoins-vote-v1
+        </Link>
+      </Stack>
+      <Stack direction={["column", "row"]} justifyContent="space-between">
+        <Text fontWeight="bold">Voting Method:</Text>
+        <Link href="#your-link-for-CCIP-011" isExternal>
+          CCIP-011
+        </Link>
+      </Stack>
+      <Box>
+        <Text fontWeight="bold" mb={2}>
+          Details:
+        </Text>
+        <Text mb={2}>
+          The CityCoins community is proposing an update to the emissions
+          schedule for current and future CityCoins. The update will
+          adjust/compress the emissions schedule with "halvings" based on a
+          doubling epoch model with an initial bonus period, in which the length
+          of each epoch is twice as long as the last following epoch 1. For more
+          details, see{" "}
+          <Link href="#your-link-for-CCIP-008" isExternal>
+            CCIP-008
+          </Link>
+          .
+        </Text>
+        <Text>
+          This also presents the opportunity to add some minor features to the
+          CityCoins protocol, including token divisibility, an upgraded VRF
+          contract with performance improvements and modification to the auth
+          contract to allow for more flexibility in the future. For more
+          details, see{" "}
+          <Link href="#your-link-for-CCIP-009" isExternal>
+            CCIP-009
+          </Link>{" "}
+          and{" "}
+          <Link href="#your-link-for-CCIP-010" isExternal>
+            CCIP-010
+          </Link>
+          .
+        </Text>
+      </Box>
+    </Stack>
+  );
 }
 
 export default Voting;
