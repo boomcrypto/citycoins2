d47efda4bad5da53952e5d5c7114b4d4532720b5
Jason Schrader <whoabuddy@users.noreply.github.com>
2023-09-01 21:10:39 -0700

    fix: add dashboard links to other sites

--- src/components/tabs/dashboard.tsx
+++ src/components/tabs/dashboard.tsx
@@ -1,4 +1,4 @@
-import { Heading, Stack } from "@chakra-ui/react";
+import { Heading, Link, Stack, Text } from "@chakra-ui/react";
 import ComingSoon from "../coming-soon";
 
 function Dashboard() {
@@ -6,6 +6,16 @@ function Dashboard() {
     <Stack spacing={4}>
       <Heading>CityCoins Dashboard</Heading>
       <ComingSoon />
+      <Text>
+        Mining statistics also available at{" "}
+        <Link isExternal href="https://miamining.com">
+          miamining.com
+        </Link>{" "}
+        and{" "}
+        <Link isExternal href="https://mining.nyc">
+          mining.nyc
+        </Link>
+      </Text>
     </Stack>
   );
 }
