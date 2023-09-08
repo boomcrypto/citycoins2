242555648abca9375ef19135f94e1618facaae32
Jason Schrader <whoabuddy@users.noreply.github.com>
2023-09-08 16:13:58 -0700

    fix: update tab theming, use new theme helpers

--- src/theme.ts
+++ src/theme.ts
@@ -2,8 +2,13 @@ import {
   extendTheme,
   type StyleFunctionProps,
   type ThemeConfig,
+  createMultiStyleConfigHelpers,
 } from "@chakra-ui/react";
+import { tabsAnatomy } from "@chakra-ui/anatomy";
 import { mode } from "@chakra-ui/theme-tools";
+
+const { definePartsStyle, defineMultiStyleConfig } =
+  createMultiStyleConfigHelpers(tabsAnatomy.keys);
 
 // Chakra theme configuration
 const config: ThemeConfig = {
@@ -17,17 +22,36 @@ const fonts = {
   body: "Open Sans, sans-serif",
 };
 
-const components = {
-  Link: {
-    baseStyle: (props: StyleFunctionProps) => ({
-      color: mode("blue.600", "blue.300")(props),
-      _hover: {
-        textDecoration: "underline",
-      },
-    }),
-  },
+const linkStyles = {
+  baseStyle: (props: StyleFunctionProps) => ({
+    color: mode("blue.600", "blue.300")(props),
+    _hover: {
+      textDecoration: "underline",
+    },
+  }),
 };
 
-const theme = extendTheme({ config, components, fonts });
+// Define the base component styles
+const tabsBaseStyle = definePartsStyle({
+  tab: {
+    fontWeight: "semibold",
+    _selected: {
+      borderTop: "5px solid",
+      color: mode("blue.600", "blue.300"),
+    },
+  },
+});
+
+// Export the component theme
+export const tabsTheme = defineMultiStyleConfig({ baseStyle: tabsBaseStyle });
+
+const theme = extendTheme({
+  config,
+  components: {
+    Link: linkStyles,
+    Tabs: tabsTheme,
+  },
+  fonts,
+});
 
 export default theme;
