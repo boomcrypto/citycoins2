ad2e4a6e9497e2137b734d33483edfe158151a0a
Jason Schrader <whoabuddy@users.noreply.github.com>
2023-09-22 21:28:45 -0700

    fix: move buttons and auth flow

--- src/components/auth/profile.tsx
+++ src/components/auth/profile.tsx
@@ -8,14 +8,20 @@ import {
   DrawerHeader,
   DrawerOverlay,
   Heading,
-  Input,
+  IconButton,
+  Stack,
   Text,
+  useColorMode,
   useDisclosure,
 } from "@chakra-ui/react";
+import { FaMoon, FaSun } from "react-icons/fa";
 import { useAtom } from "jotai";
 import { stxAddressAtom } from "../../store/stacks";
+import ClearData from "./clear-data";
+import SignOut from "./sign-out";
 
 function Profile() {
+  const { colorMode, toggleColorMode } = useColorMode();
   const { isOpen, onOpen, onClose } = useDisclosure();
   const [storedStxAddress] = useAtom(stxAddressAtom);
 
@@ -33,19 +39,38 @@ function Profile() {
         <DrawerContent>
           <DrawerCloseButton />
           <DrawerHeader>
-            {storedStxAddress.slice(0, 5)}...{storedStxAddress.slice(-5)}
+            <Stack
+              direction="row"
+              alignItems="center"
+              justifyContent="space-between"
+              mt={8}
+            >
+              <SignOut variant="outline" />
+              <ClearData variant="outline" />
+              <IconButton
+                variant="outline"
+                aria-label="Toggle color mode"
+                title="Toggle color mode"
+                icon={colorMode === "light" ? <FaMoon /> : <FaSun />}
+                onClick={toggleColorMode}
+              />
+            </Stack>
           </DrawerHeader>
           <DrawerBody>
-            <Heading size="md">Account Balances</Heading>
-            <Text>TBD</Text>
-            <Heading size="md">CityCoins User IDs</Heading>
-            <Text>TBD</Text>
+            <Stack spacing={8}>
+              <Heading size="lg" textAlign="center">
+                {storedStxAddress.slice(0, 5)}...{storedStxAddress.slice(-5)}
+              </Heading>
+              <Heading size="md">Account Balances</Heading>
+              <Text>TBD</Text>
+              <Heading size="md">CityCoins User IDs</Heading>
+              <Text>TBD</Text>
+            </Stack>
           </DrawerBody>
           <DrawerFooter>
             <Button variant="outline" mr={3} onClick={onClose}>
-              Cancel
+              Close
             </Button>
-            <Button colorScheme="blue">Save</Button>
           </DrawerFooter>
         </DrawerContent>
       </Drawer>
--- src/components/layout/page-header.tsx
+++ src/components/layout/page-header.tsx
@@ -2,29 +2,20 @@ import {
   Button,
   Flex,
   Heading,
-  IconButton,
   Menu,
   MenuButton,
   MenuItem,
   MenuList,
   Stack,
-  useColorMode,
 } from "@chakra-ui/react";
 import { useAccount } from "@micro-stacks/react";
+import { FaChevronCircleDown } from "react-icons/fa";
 import CityCoinsLogo from "./citycoins-logo";
-import SignIn from "../auth/sign-in";
-import SignOut from "../auth/sign-out";
-import ClearData from "../auth/clear-data";
-import { stxAddressAtom } from "../../store/stacks";
-import { useAtom } from "jotai";
-import { FaChevronCircleDown, FaMoon, FaSun } from "react-icons/fa";
 import Profile from "../auth/profile";
+import SignIn from "../auth/sign-in";
 
 function Header() {
   const { stxAddress } = useAccount();
-  const { colorMode, toggleColorMode } = useColorMode();
-  const [storedStxAddress] = useAtom(stxAddressAtom);
-
   return (
     <Stack align="center" direction={["column", "row"]} p={4}>
       <Flex flexGrow="1" align="center">
@@ -33,7 +24,7 @@ function Header() {
           CityCoins
         </Heading>
       </Flex>
-      <Profile />
+      {stxAddress === undefined ? <SignIn /> : <Profile />}
       <Menu>
         <MenuButton
           as={Button}
@@ -48,18 +39,6 @@ function Header() {
           <MenuItem>New York City</MenuItem>
         </MenuList>
       </Menu>
-      <ClearData variant="outline" />
-      {stxAddress === undefined ? (
-        <SignIn variant="outline" />
-      ) : (
-        <SignOut variant="outline" />
-      )}
-      <IconButton
-        aria-label="Toggle color mode"
-        title="Toggle color mode"
-        icon={colorMode === "light" ? <FaMoon /> : <FaSun />}
-        onClick={toggleColorMode}
-      />
     </Stack>
   );
 }
