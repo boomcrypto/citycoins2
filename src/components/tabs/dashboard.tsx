3a6edfc022c3a78ce45faa03ae5c7838e1083ae7
Jason Schrader <whoabuddy@users.noreply.github.com>
2024-04-22 17:39:06 -0700

    fix: apply new layout to dashboard, check for auth on tab

--- src/components/tabs/dashboard.tsx
+++ src/components/tabs/dashboard.tsx
@@ -1,21 +1,53 @@
-import { Heading, Link, Stack, Text } from "@chakra-ui/react";
-import ComingSoon from "../coming-soon";
+import {
+  Heading,
+  Stack,
+  Stat,
+  StatLabel,
+  StatNumber,
+  Text,
+} from "@chakra-ui/react";
+import { stxAddressAtom } from "../../store/stacks";
+import { useAtomValue } from "jotai";
+import SignIn from "../auth/sign-in";
 
 function Dashboard() {
+  const stxAddress = useAtomValue(stxAddressAtom);
+
   return (
     <Stack spacing={4}>
       <Heading>CityCoins Dashboard</Heading>
-      <ComingSoon />
-      <Text>
-        Mining statistics also available at{" "}
-        <Link isExternal href="https://miamining.com">
-          miamining.com
-        </Link>{" "}
-        and{" "}
-        <Link isExternal href="https://mining.nyc">
-          mining.nyc
-        </Link>
-      </Text>
+      {stxAddress ? (
+        <>
+          <Text fontWeight="bold">{stxAddress}</Text>
+          <Stack direction={["column", null, "row"]}>
+            <Stat>
+              <StatLabel>Total Transactions</StatLabel>
+              <StatNumber>XXX</StatNumber>
+            </Stat>
+            <Stat>
+              <StatLabel>Total Mining TXs</StatLabel>
+              <StatNumber>XXX</StatNumber>
+            </Stat>
+            <Stat>
+              <StatLabel>Total Mining Claim TXs</StatLabel>
+              <StatNumber>XXX</StatNumber>
+            </Stat>
+            <Stat>
+              <StatLabel>Total Stacking TXs</StatLabel>
+              <StatNumber>XXX</StatNumber>
+            </Stat>
+            <Stat>
+              <StatLabel>Total Stacking Claim TXs</StatLabel>
+              <StatNumber>XXX</StatNumber>
+            </Stat>
+          </Stack>
+        </>
+      ) : (
+        <>
+          <Text>Wallet connection required to access dashboard.</Text>
+          <SignIn />
+        </>
+      )}
     </Stack>
   );
 }
