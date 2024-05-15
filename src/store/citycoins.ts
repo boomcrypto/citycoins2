39d1dd8bdec0a6cedb2139b04e011bfd1328fc05
Jason Schrader <whoabuddy@users.noreply.github.com>
2024-05-15 14:17:53 -0700

    fix: check for mine-tokens or mine-many in legacy contracts

--- src/store/citycoins.ts
+++ src/store/citycoins.ts
@@ -40,7 +40,7 @@ export const selectedTransactionsAtom = atom<Transaction[]>((get) => {
 // helpers for identifying specific contract calls
 
 type ContractFunctionMap = {
-  [contract: string]: string;
+  [contract: string]: string | string[];
 };
 
 function checkContract(
@@ -51,22 +51,37 @@ function checkContract(
 }
 
 function checkFunctionName(
-  contractName: string,
+  contractId: string,
   functionName: string,
-  contractCallsMap: ContractFunctionMap
-) {
-  return contractCallsMap[contractName] === functionName;
+  transactionCalls: ContractFunctionMap
+): boolean {
+  const expectedFunctionName = transactionCalls[contractId];
+  if (Array.isArray(expectedFunctionName)) {
+    return expectedFunctionName.includes(functionName);
+  } else {
+    return functionName === expectedFunctionName;
+  }
 }
 
 // MINING TRANSACTIONS
 
 const miningTransactionCalls: ContractFunctionMap = {
-  "SP466FNC0P7JWTNM2R9T199QRZN1MYEDTAR0KP27.miamicoin-core-v1": "mine-tokens",
-  "SP1H1733V5MZ3SZ9XRW9FKYGEZT0JDGEB8Y634C7R.miamicoin-core-v2": "mine-tokens",
-  "SP2H8PY27SEZ03MWRKS5XABZYQN17ETGQS3527SA5.newyorkcitycoin-core-v1":
+  "SP466FNC0P7JWTNM2R9T199QRZN1MYEDTAR0KP27.miamicoin-core-v1": [
     "mine-tokens",
-  "SPSCWDV3RKV5ZRN1FQD84YE1NQFEDJ9R1F4DYQ11.newyorkcitycoin-core-v2":
+    "mine-many",
+  ],
+  "SP1H1733V5MZ3SZ9XRW9FKYGEZT0JDGEB8Y634C7R.miamicoin-core-v2": [
     "mine-tokens",
+    "mine-many",
+  ],
+  "SP2H8PY27SEZ03MWRKS5XABZYQN17ETGQS3527SA5.newyorkcitycoin-core-v1": [
+    "mine-tokens",
+    "mine-many",
+  ],
+  "SPSCWDV3RKV5ZRN1FQD84YE1NQFEDJ9R1F4DYQ11.newyorkcitycoin-core-v2": [
+    "mine-tokens",
+    "mine-many",
+  ],
   "SP8A9HZ3PKST0S42VM9523Z9NV42SZ026V4K39WH.ccd006-citycoin-mining": "mine",
   "SP8A9HZ3PKST0S42VM9523Z9NV42SZ026V4K39WH.ccd006-citycoin-mining-v2": "mine",
 };
