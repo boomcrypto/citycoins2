ca17a9d7596c7d5f5fcddbacd1f34d160fdfc7ee
Jason Schrader <whoabuddy@users.noreply.github.com>
2024-05-21 07:34:12 -0700

    fix: update layout based on tx analysis

--- src/components/tabs/mining-claims.tsx
+++ src/components/tabs/mining-claims.tsx
@@ -1,13 +1,72 @@
-import { Heading, Stack, Text } from "@chakra-ui/react";
-import { miningBlocksToClaimPerCityAtom } from "../../store/citycoins";
+import {
+  Heading,
+  Stack,
+  Stat,
+  StatLabel,
+  StatNumber,
+  Text,
+} from "@chakra-ui/react";
+import {
+  CityNames,
+  miningBlocksToClaimPerCityAtom,
+  miningClaimTransactionsAtom,
+  miningTransactionsAtom,
+} from "../../store/citycoins";
 import { useAtomValue } from "jotai";
 
 function MiningClaims() {
-  const miningBlocksToClaim = useAtomValue(miningBlocksToClaimPerCityAtom);
+  const miningTxs = useAtomValue(miningTransactionsAtom);
+  const miningClaimTxs = useAtomValue(miningClaimTransactionsAtom);
+  const { miaBlockHeights, nycBlockHeights } = useAtomValue(
+    miningBlocksToClaimPerCityAtom
+  );
   return (
     <Stack spacing={4}>
       <Heading>CityCoins Mining Claims</Heading>
-      <Text>{JSON.stringify(miningBlocksToClaim)}</Text>
+      <Stack
+        direction={["column", null, "row"]}
+        justifyContent="space-between"
+        spacing={2}
+      >
+        <Stat>
+          <StatLabel>Mining TXs</StatLabel>
+          <StatNumber>{miningTxs.length}</StatNumber>
+        </Stat>
+        <Stat>
+          <StatLabel>Mining Claim TXs</StatLabel>
+          <StatNumber>{miningClaimTxs.length}</StatNumber>
+        </Stat>
+        <Stat>
+          <StatLabel>MIA Blocks to Claim</StatLabel>
+          <StatNumber>{miaBlockHeights.length}</StatNumber>
+        </Stat>
+        <Stat>
+          <StatLabel>NYC Blocks to Claim</StatLabel>
+          <StatNumber>{nycBlockHeights.length}</StatNumber>
+        </Stat>
+      </Stack>
+      <MiningClaimsList city="mia" blockHeights={miaBlockHeights} />
+      <MiningClaimsList city="nyc" blockHeights={nycBlockHeights} />
+    </Stack>
+  );
+}
+
+interface MiningClaimsListProps {
+  city: CityNames;
+  blockHeights: number[];
+}
+
+function MiningClaimsList({ city, blockHeights }: MiningClaimsListProps) {
+  return (
+    <Stack>
+      <Text fontWeight="bold" size="lg">
+        {city.toUpperCase()}
+      </Text>
+      <Stack>
+        {blockHeights.map((blockHeight) => (
+          <Text key={`${city}-${blockHeight}`}>{blockHeight}</Text>
+        ))}
+      </Stack>
     </Stack>
   );
 }
