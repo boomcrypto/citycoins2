4569ff6454c1589e2e1d0dbee9c874dbe81af982
Jason Schrader <whoabuddy@users.noreply.github.com>
2024-08-05 10:13:41 -0700

    fix: make title easy to read

--- src/components/tabs/voting.tsx
+++ src/components/tabs/voting.tsx
@@ -41,7 +41,7 @@ function Voting() {
         <AccordionItem>
           <h2>
             <AccordionButton>
-              <VoteTitle title="Vote 9: Stacking PoX-4" status="pending" />
+              <VoteTitle title="Vote 9: Stacking with PoX-4" status="pending" />
             </AccordionButton>
           </h2>
           <AccordionPanel pb={4}>
